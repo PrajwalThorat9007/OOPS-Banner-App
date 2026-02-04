17838d513562e609c510ec660237485ca9dde040
PrajwalThorat9007 <prajwalthorat9007@gmail.com>
2026-02-04 15:53:39 +0530

    [Prajwal]:UC5:Inline array initialization for banner

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,12 +1,12 @@
 /*
-O0PSBannerApp UC4 - Render OOPS as Banner using String Array and Loop
+OOPSBannerApp UC5 - Render 00PS as Banner using Inline Array Initialization
 
-This use case improves upon UC3 by using a String array to store banner lines
-and iterating through them with a for-each loop, eliminating hardcoded print
-statements and improving modularity and reusability.
+This use case extends UC4 by defining and populating the String array in a more
+concise way at the time of declaration using String.join() method to create each
+line of the banner. This further enhances code readability and maintainability.
 
 @author Developer
-@version 4
+@version 5.0
 */
 
 public class OOPSBannerApp {
@@ -27,6 +27,7 @@ public class OOPSBannerApp {
             System.out.println(line);
         }
     }
+
 
 
 
